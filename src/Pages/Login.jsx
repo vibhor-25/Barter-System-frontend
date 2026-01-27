c026e12035c2fe02b8a04e410efbb82634cfcaaf
Shreyas Murali <25b2715@iitb.ac.in>
2026-01-27 09:10:40 +0530

    User API Integration

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,49 @@
-import {Link} from 'react-router-dom';
+import {data, Link} from 'react-router-dom';
 import {useNavigate} from 'react-router-dom';
+import {useState} from 'react';
+import axios from 'axios';
+
+async function login_request(data){
+    // const response = await axios({
+    //   method: "POST",
+    //   url: "login/",
+    //   baseURL: "http://127.0.0.1:8000/api/auth/user/",
+    //   data: data,
+    // }).then((rec_data) => {
+    //   // console.log(rec_data.data)
+    //   if (rec_data.status === 200) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // });
+
+    await axios.post(
+       "http://localhost:8000/api/auth/user/login/",
+        data,
+        { withCredentials: true, },
+    ).then((rec_data) => {
+      // console.log(rec_data.data)
+      if (rec_data.status === 200) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+}
+
 function Login() {
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: ""
+    })
+
     const navigate = useNavigate();
-  const handleDivClick = () => {
-    navigate('/home');
-  };
+    const handleDivClick = async () => {
+        const login_success = login_request(loginData);
+
+        if(login_success) navigate('/home');
+    };
     return(
         <>
         <img className='background' src='../../public/images/backgroundimg.png' alt=''/>
@@ -17,12 +56,16 @@ function Login() {
 
                 <div className='emaildiv'>
                     <h1 className='emailtext'>Email</h1>
-                    <input className='inputbox' placeholder='eg. abc'></input>
+                    <input className='inputbox' placeholder='eg. abc' value={loginData.email} onChange={(e) => {
+                        setLoginData({...loginData, email: e.target.value})
+                    }}></input>
                 </div>
 
                 <div className='passworddiv'>
                     <h1 className='passwordtext'>Password</h1>
-                    <input className='inputbox' placeholder='eg. abc'></input>
+                    <input className='inputbox' placeholder='eg. abc' value={loginData.password} onChange={(e) => {
+                        setLoginData({...loginData, password: e.target.value})
+                    }}></input>
                 </div>
 
                 <div className='forgotdiv'>
